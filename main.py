7b72e08ed03d3c7b47a6e55fb8a24a4b9391bbef
Steven A. deCsesznak <105519651+AdeCsesznak@users.noreply.github.com>
2024-03-01 18:22:59 -0600

    Updated to 1 Table

--- main.py
+++ main.py
@@ -1,28 +1,9 @@
 import reddit
-import utilities as util
-from datetime import datetime
-import time
 
-def format_n_save_reddit(json_data, community):
-    df = util.json_to_dataframe(json_data)
-    transposed_df = df.transpose()
 
-    df = transposed_df.reset_index()
-
-    current_datetime = datetime.now()
-    df['datetime'] = current_datetime
-
-    df = df[df['index'] == 'data']
-    df = df[['datetime', 'accounts_active', 'active_user_count', 'subscribers']]
-    util.append_df_to_sql(df, community)
 
 def main():
-    reddit_communities = util.read_table('reddit_communities')
-
-    for community in reddit_communities['Reddit_Community']:
-        json_response = reddit.get_community_info(community)
-        format_n_save_reddit(json_response, community)
-        time.sleep(5)
+    reddit.get_reddit_main()
 
 
 
--- reddit.py
+++ reddit.py
@@ -1,8 +1,28 @@
 import requests
+from datetime import datetime
+import time
+import pandas as pd
+import utilities as util
+
+def format_n_save_reddit(json_data, subreddit, token):
+    df = pd.DataFrame(json_data)
+    transposed_df = df.transpose()
+
+    df = transposed_df.reset_index()
+
+    current_datetime = datetime.now()
+    df['datetime'] = current_datetime
+    
+    df['subreddit'] = subreddit
+    df['token'] = token
+
+    df = df[df['index'] == 'data']
+    df = df[['datetime', 'subreddit', 'token', 'accounts_active', 'active_user_count', 'subscribers']]
+    
+    return df
 
 
-
-def get_community_info(community_name):
+def get_subreddit_info(community_name):
 
     api_url = f'https://www.reddit.com/r/{community_name}/about.json'
     headers = {'User-Agent': 'YourBot/0.2'}  # Add a user-agent header to avoid being blocked
@@ -17,3 +37,14 @@ def get_community_info(community_name):
         print("Error:", response.status_code)
         return None
 
+def get_reddit_main():
+    # Read in Reddit CSV
+    df = pd.read_csv(r"input_data/reddit_communities.csv")
+    
+    # Loop through Rows
+    for index, row in df.iterrows():
+        print(f'Start - {row["token"]}')
+        json_response = get_subreddit_info(row['subreddit'])
+        subreddit_df = format_n_save_reddit(json_response, row['subreddit'], row['token'])
+        util.append_df_to_sql(subreddit_df, "reddit")
+        #time.sleep(1)
